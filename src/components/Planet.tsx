e7cb3697089241483aa449c5823d5e78cdfb4b47
Bruno Hirae <brunodeluca117@outlook.com>
2024-12-16 14:56:18 -0300

    add planet variants

--- src/components/Planet.tsx
+++ src/components/Planet.tsx
@@ -3,14 +3,14 @@ import { cva } from "class-variance-authority";
 const classes = cva("bg-gradient-to-b to-gray-950 rounded-full", {
   variants: {
     size: {
-      sm: "",
-      md: "",
+      sm: "size-4",
+      md: "size-6",
       lg: "size-8",
     },
     color: {
       violet: "from-violet-400",
-      teal: "bg-teal-300",
-      fuchsia: "bg-fuchsia-400",
+      teal: "from-teal-400",
+      fuchsia: "from-fuchsia-400",
     },
   },
   defaultVariants: {
