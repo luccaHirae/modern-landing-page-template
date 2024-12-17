c2243ad9981647d1ce69a89bcdee16bc9ba94886
Bruno Hirae <brunodeluca117@outlook.com>
2024-12-17 11:09:52 -0300

    add border top to section

--- src/components/SectionBorder.tsx
+++ src/components/SectionBorder.tsx
@@ -1,16 +1,39 @@
+import { faPlus } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { twMerge } from "tailwind-merge";
+
+export interface SectionBorderProps
+  extends React.HTMLAttributes<HTMLDivElement> {
+  borderTop?: boolean;
+}
 
 export const SectionBorder = ({
   className,
+  borderTop = false,
+  children,
   ...props
-}: React.HTMLAttributes<HTMLDivElement>) => {
+}: SectionBorderProps) => {
   return (
     <div
       className={twMerge(
-        "border-l border-r border-[var(--color-border)]",
+        "border-l border-r border-[var(--color-border)] relative",
+        borderTop && "border-t",
         className
       )}
       {...props}
-    />
+    >
+      {borderTop && (
+        <>
+          <div className="absolute top-0 left-0 -translate-x-1/2 -translate-y-1/2">
+            <FontAwesomeIcon icon={faPlus} className="size-4 text-gray-200" />
+          </div>
+          <div className="absolute top-0 right-0 translate-x-1/2 -translate-y-1/2">
+            <FontAwesomeIcon icon={faPlus} className="size-4 text-gray-200" />
+          </div>
+        </>
+      )}
+
+      {children}
+    </div>
   );
 };
