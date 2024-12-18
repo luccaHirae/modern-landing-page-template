988a419222e83893a9611580e9a201824c68fc72
Bruno Hirae <brunodeluca117@outlook.com>
2024-12-18 13:39:53 -0300

    add footer section

--- src/sections/Footer.tsx
+++ src/sections/Footer.tsx
@@ -4,6 +4,7 @@ import {
   faXTwitter,
   faDiscord,
 } from "@fortawesome/free-brands-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 export const navItems = [
   {
@@ -28,22 +29,58 @@ export const socialLinks = [
   {
     name: "Youtube",
     icon: faYoutube,
-    href: "#youtube",
+    href: "#",
   },
   {
     name: "X",
     icon: faXTwitter,
-    href: "#x",
+    href: "#",
   },
   {
     name: "Discord",
     icon: faDiscord,
-    href: "#discord",
+    href: "#",
   },
 ];
 
 export const Footer = () => {
-  return <footer>footer section</footer>;
+  return (
+    <footer className="border-t border-[var(--color-border)]">
+      <div className="container py-8">
+        <div className="flex flex-col lg:flex-row lg:justify-between items-center gap-8">
+          <div className="font-extrabold text-2xl">sphereal.ai</div>
+
+          <nav className="flex flex-col md:flex-row gap-8 md:gap-16 items-center">
+            {navItems.map((item) => (
+              <a
+                key={item.name}
+                href={item.href}
+                className="uppercase text-xs tracking-widest font-bold text-gray-400"
+              >
+                {item.name}
+              </a>
+            ))}
+          </nav>
+        </div>
+
+        <div className="mt-16 flex flex-col md:flex-row-reverse md:justify-between items-center gap-8">
+          <div className="flex justify-center gap-6">
+            {socialLinks.map((link) => (
+              <a key={link.name} href={link.href}>
+                <div className="size-10 rounded-full bg-gray-900 inline-flex items-center justify-center">
+                  <FontAwesomeIcon icon={link.icon} className="size-4" />
+                </div>
+              </a>
+            ))}
+          </div>
+
+          <p className="text-gray-500 text-sm">
+            &copy; Sphereal.AI, All rights reserved.
+          </p>
+        </div>
+      </div>
+    </footer>
+  );
 };
 
 export default Footer;
