f78d27433facd40b2a15132ddced11808213b607
Bruno Hirae <brunodeluca117@outlook.com>
2024-12-19 12:01:22 -0300

    add primary button animation

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,3 +1,12 @@
+"use client";
+
+import { useEffect, useState } from "react";
+import {
+  animate,
+  motion,
+  useMotionTemplate,
+  useMotionValue,
+} from "framer-motion";
 import { cva, VariantProps } from "class-variance-authority";
 
 const classes = cva(
@@ -21,21 +30,46 @@ const classes = cva(
   }
 );
 
-export interface ButtonProps
-  extends Omit<React.ButtonHTMLAttributes<HTMLButtonElement>, "disabled">,
-    VariantProps<typeof classes> {}
+export interface ButtonProps extends VariantProps<typeof classes> {
+  className?: string;
+  disabled?: boolean;
+  children: React.ReactNode;
+}
 
 export const Button = ({
   className,
-  variant,
+  variant = "primary",
+  children,
   disabled,
   ...props
 }: ButtonProps) => {
+  const [isHovered, setIsHovered] = useState(false);
+
+  const angle = useMotionValue(45);
+
+  const background = useMotionTemplate`linear-gradient(var(--color-gray-950),var(--color-gray-950)) padding-box, conic-gradient(from ${angle}deg,var(--color-violet-400),var(--color-fuchsia-400),var(--color-amber-300),var(--color-teal-300),var(--color-violet-400)) border-box`;
+
+  useEffect(() => {
+    if (isHovered) {
+      animate(angle, angle.get() + 360, {
+        duration: 1,
+        repeat: Infinity,
+        ease: "linear",
+      });
+    } else {
+      animate(angle, 45, { duration: 0.5 });
+    }
+  }, [angle, isHovered]);
+
   return (
-    <button
+    <motion.button
+      onMouseEnter={() => setIsHovered(true)}
+      onMouseLeave={() => setIsHovered(false)}
       className={classes({ variant, disabled, className })}
-      disabled={disabled ?? undefined}
+      style={variant === "primary" ? { background: background } : undefined}
       {...props}
-    />
+    >
+      {children}
+    </motion.button>
   );
 };
