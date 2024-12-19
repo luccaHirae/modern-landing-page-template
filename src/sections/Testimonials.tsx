5e96ea328eab7ff1e2410175652fbea635a0b9b0
Bruno Hirae <brunodeluca117@outlook.com>
2024-12-19 12:26:42 -0300

    animate testimonials section

--- src/sections/Testimonials.tsx
+++ src/sections/Testimonials.tsx
@@ -1,11 +1,12 @@
 "use client";
 
-import React, { Fragment } from "react";
+import React, { useState } from "react";
 import Image from "next/image";
 import AshwinSantiago from "@/assets/images/ashwin-santiago.jpg";
 import AlecWhitten from "@/assets/images/alec-whitten.jpg";
 import ReneWells from "@/assets/images/rene-wells.jpg";
 import MollieHall from "@/assets/images/mollie-hall.jpg";
+import { AnimatePresence, LayoutGroup, motion } from "framer-motion";
 import { SectionBorder } from "@/components/SectionBorder";
 import { SectionContent } from "@/components/SectionContent";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
@@ -42,63 +43,93 @@ export const testimonials = [
   },
 ];
 
-const SELECTED_TESTIMONIAL_INDEX = 0;
-
 export const Testimonials = () => {
+  const [selectedTestimonialIndex, setSelectedTestimonialIndex] = useState(0);
+
   return (
     <section id="testimonials">
       <div className="container">
         <SectionBorder borderTop>
           <SectionContent>
-            <div className="border-gradient rounded-3xl px-6 md:px-10 lg:px-16 py-16 lg:py-24 relative flex flex-col md:flex-row items-center gap-12 md:mx-10 lg:mx-20">
-              <FontAwesomeIcon
-                icon={faQuoteLeft}
-                className="absolute size-20 text-violet-400 top-0 left-6 md:left-10 lg:left-16 -translate-y-1/2"
-              />
+            <LayoutGroup>
+              <motion.div
+                layout
+                className="border-gradient rounded-3xl px-6 md:px-10 lg:px-16 py-16 lg:py-24 relative flex flex-col md:flex-row gap-12 md:mx-10 lg:mx-20"
+              >
+                <FontAwesomeIcon
+                  icon={faQuoteLeft}
+                  className="absolute size-20 text-violet-400 top-0 left-6 md:left-10 lg:left-16 -translate-y-1/2"
+                />
 
-              {testimonials.map((testimonial, index) => (
-                <Fragment key={testimonial.name}>
-                  {SELECTED_TESTIMONIAL_INDEX === index && (
-                    <blockquote
-                      key={testimonial.name}
-                      className="flex flex-col lg:flex-row gap-12"
-                    >
-                      <p className="text-xl md:text-2xl font-medium">
-                        {testimonial.quote}
-                      </p>
-                      <cite className="not-italic lg:text-right">
-                        <Image
-                          src={testimonial.image}
-                          alt={testimonial.name}
-                          className="rounded-xl size-28 max-w-none"
-                        />
+                <AnimatePresence mode="wait" initial={false}>
+                  {testimonials.map((testimonial, index) =>
+                    selectedTestimonialIndex === index ? (
+                      <motion.blockquote
+                        layout
+                        initial={{
+                          opacity: 0,
+                          y: 25,
+                        }}
+                        animate={{
+                          opacity: 1,
+                          y: 0,
+                        }}
+                        exit={{
+                          opacity: 0,
+                          y: -25,
+                        }}
+                        transition={{
+                          duration: 0.5,
+                        }}
+                        key={testimonial.name}
+                        className="flex flex-col lg:flex-row gap-12"
+                      >
+                        <p className="text-xl md:text-2xl font-medium">
+                          {testimonial.quote}
+                        </p>
+                        <cite className="not-italic lg:text-right">
+                          <Image
+                            src={testimonial.image}
+                            alt={testimonial.name}
+                            className="rounded-xl size-28 max-w-none"
+                          />
 
-                        <div className="font-bold mt-4">{testimonial.name}</div>
+                          <div className="font-bold mt-4">
+                            {testimonial.name}
+                          </div>
 
-                        <div className="text-xs text-gray-400 mt-2">
-                          {testimonial.title}
-                        </div>
-                      </cite>
-                    </blockquote>
+                          <div className="text-xs text-gray-400 mt-2">
+                            {testimonial.title}
+                          </div>
+                        </cite>
+                      </motion.blockquote>
+                    ) : null
                   )}
-                </Fragment>
-              ))}
+                </AnimatePresence>
 
-              <div className="flex gap-2 md:flex-col justify-center">
-                {testimonials.map((testimonial, index) => (
-                  <div
-                    key={testimonial.name}
-                    className="size-6 relative isolate inline-flex items-center justify-center"
-                  >
-                    {SELECTED_TESTIMONIAL_INDEX === index && (
-                      <div className="absolute border-gradient size-full rounded-full -z-10"></div>
-                    )}
+                <motion.div
+                  layout="position"
+                  className="flex gap-2 md:flex-col"
+                >
+                  {testimonials.map((testimonial, index) => (
+                    <div
+                      key={testimonial.name}
+                      onClick={() => setSelectedTestimonialIndex(index)}
+                      className="size-6 relative isolate inline-flex items-center justify-center cursor-pointer"
+                    >
+                      {selectedTestimonialIndex === index && (
+                        <motion.div
+                          layoutId="testimonial-dot"
+                          className="absolute border-gradient size-full rounded-full -z-10"
+                        ></motion.div>
+                      )}
 
-                    <div className="size-1.5 bg-gray-200 rounded-full"></div>
-                  </div>
-                ))}
-              </div>
-            </div>
+                      <div className="size-1.5 bg-gray-200 rounded-full"></div>
+                    </div>
+                  ))}
+                </motion.div>
+              </motion.div>
+            </LayoutGroup>
           </SectionContent>
         </SectionBorder>
       </div>
