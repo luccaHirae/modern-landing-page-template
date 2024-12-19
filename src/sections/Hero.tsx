18ea18a74e25e429dce5aaabf3457a92a8a63742
Bruno Hirae <brunodeluca117@outlook.com>
2024-12-19 10:54:55 -0300

    add hero animations

--- src/sections/Hero.tsx
+++ src/sections/Hero.tsx
@@ -1,16 +1,88 @@
+"use client";
+
 import Image from "next/image";
 import robotImg from "@/assets/images/robot.jpg";
 import underlineImg from "@/assets/images/underline.svg?url";
-import Loader from "@/assets/images/loader.svg";
+import Loader from "@/assets/images/loader-animated.svg";
+import { useEffect, useRef, useState } from "react";
+import {
+  motion,
+  useMotionValue,
+  useScroll,
+  useSpring,
+  useTransform,
+} from "framer-motion";
 import { Button } from "@/components/Button";
 import { Orbit } from "@/components/Orbit";
 import { Planet } from "@/components/Planet";
 import { SectionBorder } from "@/components/SectionBorder";
 import { SectionContent } from "@/components/SectionContent";
 
+const useMousePosition = () => {
+  const [innerWidth, setInnerWidth] = useState(1);
+  const [innerHeight, setInnerHeight] = useState(1);
+
+  const clientX = useMotionValue(0);
+  const clientY = useMotionValue(0);
+
+  const xProgress = useTransform(clientX, [0, innerWidth], [0, 1]);
+  const yProgress = useTransform(clientY, [0, innerHeight], [0, 1]);
+
+  useEffect(() => {
+    const handleResize = () => {
+      setInnerWidth(window.innerWidth);
+      setInnerHeight(window.innerHeight);
+    };
+
+    window.addEventListener("resize", handleResize);
+
+    return () => {
+      window.removeEventListener("resize", handleResize);
+    };
+  }, []);
+
+  useEffect(() => {
+    const handleMouseMove = (e: MouseEvent) => {
+      clientX.set(e.clientX);
+      clientY.set(e.clientY);
+    };
+
+    window.addEventListener("mousemove", handleMouseMove);
+
+    return () => {
+      window.removeEventListener("mousemove", handleMouseMove);
+    };
+  }, [clientX, clientY]);
+
+  return { xProgress, yProgress };
+};
+
 export const Hero = () => {
+  /* Parallax Animation */
+  const sectionRef = useRef<HTMLDivElement>(null);
+
+  const { scrollYProgress } = useScroll({
+    target: sectionRef,
+    offset: ["end start", "start end"],
+  });
+
+  const transformedY = useTransform(scrollYProgress, [0, 1], [200, -200]);
+
+  /* Mouse Position Animation */
+  const { xProgress, yProgress } = useMousePosition();
+
+  const springX = useSpring(xProgress);
+  const springY = useSpring(yProgress);
+
+  const translateLargeX = useTransform(springX, [0, 1], ["-25%", "25%"]);
+  const translateLargeY = useTransform(springY, [0, 1], ["-25%", "25%"]);
+  const translateMediumX = useTransform(springX, [0, 1], ["-50%", "50%"]);
+  const translateMediumY = useTransform(springY, [0, 1], ["-50%", "50%"]);
+  const translateSmallX = useTransform(springX, [0, 1], ["-200%", "200%"]);
+  const translateSmallY = useTransform(springY, [0, 1], ["-200%", "200%"]);
+
   return (
-    <section>
+    <section ref={sectionRef}>
       <div className="container">
         <SectionBorder>
           <SectionContent className="relative isolate [mask-image:linear-gradient(to_bottom,transparent,black_10%,black_90%,transparent)]">
@@ -67,42 +139,80 @@ export const Hero = () => {
 
             <div className="relative isolate max-w-5xl mx-auto">
               <div className="absolute left-1/2 top-0">
-                <Planet
-                  size="lg"
-                  color="violet"
-                  className="-translate-x-[316px] -translate-y-[76px] rotate-135"
-                />
+                <motion.div
+                  style={{
+                    x: translateLargeX,
+                    y: translateLargeY,
+                  }}
+                >
+                  <Planet
+                    size="lg"
+                    color="violet"
+                    className="-translate-x-[316px] -translate-y-[76px] rotate-135"
+                  />
+                </motion.div>
 
-                <Planet
-                  size="lg"
-                  color="violet"
-                  className="-translate-y-[188px] translate-x-[334px] -rotate-135"
-                />
+                <motion.div
+                  style={{
+                    x: translateLargeX,
+                    y: translateLargeY,
+                  }}
+                >
+                  <Planet
+                    size="lg"
+                    color="violet"
+                    className="-translate-y-[188px] translate-x-[334px] -rotate-135"
+                  />
+                </motion.div>
 
-                <Planet
-                  size="sm"
-                  color="fuchsia"
-                  className="-translate-y-[372px] -translate-x-[508px] rotate-135"
-                />
+                <motion.div
+                  style={{
+                    x: translateSmallX,
+                    y: translateSmallY,
+                  }}
+                >
+                  <Planet
+                    size="sm"
+                    color="fuchsia"
+                    className="-translate-y-[372px] -translate-x-[508px] rotate-135"
+                  />
+                </motion.div>
 
-                <Planet
-                  size="md"
-                  color="teal"
-                  className="-translate-y-[342px] translate-x-[488px] -rotate-135"
-                />
+                <motion.div
+                  style={{
+                    x: translateMediumX,
+                    y: translateMediumY,
+                  }}
+                >
+                  <Planet
+                    size="md"
+                    color="teal"
+                    className="-translate-y-[342px] translate-x-[488px] -rotate-135"
+                  />
+                </motion.div>
               </div>
 
               <div className="absolute top-[30%] left-0 -translate-x-10 z-10 hidden lg:block">
-                <div className="bg-gray-800/70 backdrop-blur-md border-gray-700 rounded-xl p-4 w-72">
+                <motion.div
+                  className="bg-gray-800/70 backdrop-blur-md border-gray-700 rounded-xl p-4 w-72"
+                  style={{
+                    y: transformedY,
+                  }}
+                >
                   <div>Can you generate an incredible landing page?</div>
                   <div className="text-right text-gray-400 text-sm font-semibold">
                     1m ago
                   </div>
-                </div>
+                </motion.div>
               </div>
 
               <div className="absolute top-[50%] right-0 translate-x-10 z-10 hidden lg:block">
-                <div className="bg-gray-800/70 backdrop-blur-md border-gray-700 rounded-xl p-4 w-72">
+                <motion.div
+                  className="bg-gray-800/70 backdrop-blur-md border-gray-700 rounded-xl p-4 w-72"
+                  style={{
+                    y: transformedY,
+                  }}
+                >
                   <div>
                     <strong>Brainwave:</strong> I created based on your
                     requirements!
@@ -110,7 +220,7 @@ export const Hero = () => {
                   <div className="text-right text-gray-400 text-sm font-semibold">
                     Just now
                   </div>
-                </div>
+                </motion.div>
               </div>
 
               <div className="mt-20 rounded-2xl border-2 overflow-hidden border-gradient relative">
@@ -121,7 +231,8 @@ export const Hero = () => {
                     <Loader className="text-violet-400 fill-violet-400" />
 
                     <div className="font-semibold text-xl text-gray-100">
-                      AI is generating <span>|</span>
+                      AI is generating{" "}
+                      <span className="animate-cursor-blink">|</span>
                     </div>
                   </div>
                 </div>
