b5d95e89c3ac3bb302bd84bbde89e8339df10f98
Bruno Hirae <brunodeluca117@outlook.com>
2024-12-19 11:20:53 -0300

    add companies section animation

--- src/sections/Companies.tsx
+++ src/sections/Companies.tsx
@@ -1,9 +1,12 @@
+"use client";
+
 import AcmeCorpLogo from "@/assets/images/acme-corp-logo.svg";
 import EchoValleyLogo from "@/assets/images/echo-valley-logo.svg";
 import QuantumLogo from "@/assets/images/quantum-logo.svg";
 import PulseLogo from "@/assets/images/pulse-logo.svg";
 import OutsideLogo from "@/assets/images/outside-logo.svg";
 import CelestialLogo from "@/assets/images/celestial-logo.svg";
+import { motion } from "framer-motion";
 import { SectionBorder } from "@/components/SectionBorder";
 import { SectionContent } from "@/components/SectionContent";
 
@@ -45,13 +48,22 @@ export const Companies = () => {
             </h2>
 
             <div className="flex mt-20 overflow-x-clip -mx-4 lg:-mx-8">
-              <div className="flex flex-none gap-18 md:gap-36 px-18">
-                {companies.map(({ logo: Logo, name }) => (
-                  <div key={name}>
+              <motion.div
+                className="flex flex-none gap-18 md:gap-36 px-9 md:px-18"
+                initial={{ x: 0 }}
+                animate={{ x: "-50%" }}
+                transition={{
+                  repeat: Infinity,
+                  duration: 10,
+                  ease: "linear",
+                }}
+              >
+                {[...companies, ...companies].map(({ logo: Logo }, index) => (
+                  <div key={index}>
                     <Logo className="h-8" />
                   </div>
                 ))}
-              </div>
+              </motion.div>
             </div>
           </SectionContent>
         </SectionBorder>
